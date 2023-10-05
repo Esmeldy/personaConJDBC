28115c88d7997cec9a5acef33d4afd5b68328155
Esmeldy <67498436+Esmeldy@users.noreply.github.com>
2023-10-05 14:20:03 +0200

    Testeo correcto

--- src/test/java/TestPersonaDAO.java
+++ src/test/java/TestPersonaDAO.java
@@ -2,19 +2,25 @@ import datos.PersonaDaoJDBC;
 import domain__entity.PersonaDTO;
 import datos.PersonaDao;
 
+import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
 
 public class TestPersonaDAO {
     public static void main(String[] args) {
         PersonaDao personaDAO = new PersonaDaoJDBC();
         PersonaDTO persona = new PersonaDTO(
-                "Mariano",
-                "Rajoy",
-                "mrajoy@Gmail.com",
-                "969669596"
+                "Luka",
+                "Modrid",
+                "lmod@Gmail.com",
+                "999669596"
         );
 
-     //   personaDAO.insert(persona);
+        try {
+            personaDAO.insert(persona);
+        } catch (SQLException e) {
+            System.out.println("Error en la BBDD"+ Arrays.toString(e.getStackTrace()));
+        }
         List<PersonaDTO> personaDTOList = personaDAO.getPersonas();
         personaDTOList.forEach(System.out::println);
     }
